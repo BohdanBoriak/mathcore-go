e19bf0f5d357563a9f4994ea9865c23fcbb2816f
BohdanBoriak <lightraven72@gmail.com>
2026-03-12 09:32:17 +0200

    Add: menu and users info gathering

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"math/rand"
+	"mathcore-go/domain"
 	"strconv"
 	"time"
 )
@@ -10,14 +11,42 @@ import (
 const totalPoints = 100
 const pointsPerQuestion = 20
 
+var id uint64 = 1
+
+func menu() {
+	fmt.Println("1. Грати")
+	fmt.Println("2. Рейтинг")
+	fmt.Println("3. Вийти")
+}
+
 func main() {
 	fmt.Println("Вітаю у грі MathCore \\m/")
 
-	for i := 5; i > 0; i-- {
-		fmt.Println(i)
-		time.Sleep(1 * time.Second)
-	}
+	var users []domain.User
 
+	for {
+		menu()
+
+		choice := ""
+		fmt.Scan(&choice)
+
+		switch choice {
+		case "1":
+			u := play()
+			users = append(users, u)
+		case "2":
+			for _, u := range users {
+				fmt.Printf("Id: %v Name: %s Time: %v",
+					u.Id, u.Name, u.Time)
+			}
+		case "3":
+			return
+		default:
+		}
+	}
+}
+
+func play() domain.User {
 	fmt.Println("Хай щастить!")
 	myPoints := 0
 	start := time.Now()
@@ -46,5 +75,18 @@ func main() {
 	end := time.Now()
 	timeSpent := end.Sub(start)
 	fmt.Printf("Вітаю! Ти впорався(лась) за %v!", timeSpent)
-	time.Sleep(5 * time.Second)
+
+	fmt.Println("Введіть ім'я: ")
+	name := ""
+
+	fmt.Scan(&name)
+
+	user := domain.User{
+		Id:   id,
+		Name: name,
+		Time: timeSpent,
+	}
+	id++
+
+	return user
 }
